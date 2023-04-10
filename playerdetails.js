1aadc794421badb8d9e412bde470f1b669e9c527
adurthi kumarsurya <121813015+asurya185@users.noreply.github.com>
2023-04-10 19:27:47 +0530

    Add files via upload

---
+++ playerdetails.js
@@ -0,0 +1,119 @@
+let urlData = location.href;
+let newUrl = new URL(urlData);
+let playerUrl = newUrl.searchParams.get("name");
+console.log(playerUrl)
+
+
+
+
+teamsDetails = JSON.parse(localStorage.getItem("teamArray"));
+playersDetails = JSON.parse(localStorage.getItem("playerArray"));
+
+let playerData = document.getElementById("player-details-con")
+for (var i = 0; i < playersDetails.length; i++) {
+    if (playersDetails[i].playerName == playerUrl) {
+        var playingOrN = ""
+        if (playersDetails[i].isPlaying == true) {
+            playingOrN = "Playing"
+        }
+        else {
+            playingOrN = "On Bench"
+        }
+        playerData.innerHTML += `
+
+        <div id="player_detail-img">
+        <img src="${playersDetails[i].playerImg}" alt="">
+    </div>
+        <div id="new-player-det">
+        <table>
+        <tr>
+            <td>Player Name  </td>
+            <td>${playersDetails[i].playerName}  </td>
+        </tr>
+    <table>
+        <tr>
+            <td> Team Name </td>
+            <td> ${playersDetails[i].playerTeam}  </td>
+        </tr>
+    <table>
+        <tr>
+            <td> Team Code </td>
+            <td>${playersDetails[i].from}   </td>
+        </tr>
+    <table>
+        <tr>
+            <td> Worth </td>
+            <td>${playersDetails[i].price}  </td>
+        </tr>
+    <table>
+        <tr>
+            <td> Is playing </td>
+            <td> ${playingOrN} </td>
+        </tr>
+    </table>
+    </div>
+    
+        `
+    }
+}
+
+{
+   "id"; 0,
+   "playerName"; "Hardik Pandya",
+   "from"; "GT",
+   "price"; "12.50 Cr",
+   "isPlaying"; true,
+ "description"; "All-rounder",
+   "playerImg"; 'https://assets.iplt20.com/ipl/IPLHeadshot2022/2740.png',
+   "playerTeam"; "Gujarat Titans"
+}
+
+{
+    "id"; 1,
+    "playerName"; "Virat Kohli",
+    "from"; "RCB",
+    "price"; "8.00 Cr",
+    "isPlaying"; true,
+    "description"; "Batsman"
+    }
+    {
+    "id"; 2,
+    "playerName"; "Yuvraj Singh",
+    "from"; "MI",
+    "price"; "1.00 Cr",
+    "isPlaying"; false,
+    "description"; "Batsman"
+    }
+    {
+    "id"; 3,
+    "playerName"; "Chris Morris",
+    "from"; "RR",
+    "price"; "16.25 Cr",
+    "isPlaying"; true,
+    "description"; "All-rounder"
+    }
+    {
+    "id"; 4,
+    "playerName"; "Glenn Maxwell",
+    "from"; "RCB",
+    "price"; "14.25",
+    "isPlaying"; true,
+    "description"; "All-rounder"
+    }
+    {
+    "id"; 5,
+    "playerName"; "Rohit Sharma",
+    "from"; "MI",
+    "price"; "6.50 Cr",
+    "isPlaying"; true,
+    "description"; "BatsMan"
+    }
+    {
+    "id"; 6,
+    "playerName"; "Ishan Kishan",
+    "from"; "MI",
+    "price"; "2.50 Cr",
+    "isPlaying"; true,
+    "description"; "BatsMan"
+    }
+    
